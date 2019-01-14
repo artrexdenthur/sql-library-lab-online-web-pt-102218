5b3366f38ed4fff50b949dd0e4b727deae628e41
Paul Ashour <paul.ashour@gmail.com>
2019-01-14 05:17:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,10 @@ INSERT INTO series VALUES (2, "S2", 2, 2);
 
 -- subgenres: id, name
 
+INSERT INTO subgenres VALUES (1, "Sci-fi");
+INSERT INTO subgenres VALUES (2, "Fantasy");
+
+
 -- authors: id, name
 
 -- books: id, title, year, series_id
