38d47fdff5a244991140a79b3896bf0cfa4e5e73
Paul Ashour <paul.ashour@gmail.com>
2019-01-14 05:14:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,13 @@
--- series: 
+-- series: id, title, author_id, subgenre_id
+
+INSERT 
+
+-- subgenres: id, name
+
+-- authors: id, name
+
+-- books: id, title, year, series_id
+
+-- characters: id, name, motto, species, author_id, series_id
+
+-- character_books: id, character_id, book_id
