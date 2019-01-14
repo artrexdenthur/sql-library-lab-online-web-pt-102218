bfcbcbbe54a6c698e6842e33dfba03afff20b395
Paul Ashour <paul.ashour@gmail.com>
2019-01-14 05:26:13 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -29,30 +29,8 @@ INSERT INTO characters VALUES (4, "Felix", "Fix it", "Pixels", 1, 1);
 INSERT INTO characters VALUES (5, "Oscar", "Trash it", "Muppet", 2, 2);
 INSERT INTO characters VALUES (6, "Tedd", "Change it", "Goddess", 2, 2);
 INSERT INTO characters VALUES (7, "Jamie", "Mail it", "Thespian", 2, 2);
-INSERT INTO characters VALUES (8, "");
+INSERT INTO characters VALUES (8, "Bismuth", "Upgrade it", "Gem", 2, 2);
 
-Buy it, use it, break it, fix it,
-Trash it, change it, mail, upgrade it,
-Charge it, point it, zoom it, press it,
-Snap it, work it, quick, erase it,
-Write it, cut it, paste it, save it,
-Load it, check it, quick, rewrite it
-Plug it, play it, burn it, rip it,
-Drag and drop it, zip, unzip it,
-Lock it, fill it, curl it, find it,
-View it, code it, jam, unlock it 
-Surf it, scroll it, pose it, click it
-Cross it, crack it, twitch, update it,
-Name it, read it, tune it, print it,
-Scan it, send it, fax, rename it,
-Touch it, bring it, pay it, watch it,
-Turn it, leave it, stop, format it.
-Buy it, use it, break it, fix it,
-Trash it, change it, mail, upgrade it,
-Charge it, point it, zoom it, press it,
-Snap it, work it, quick, erase it,
-Write it, cut it, paste it, save it,
-Load…
 
 -- character_books: id, character_id, book_id
 
